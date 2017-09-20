40a6dc56c4cc2fafd83c3ad69b79bd9f5efc159d
Joseph Tomlinson and lowesusannah@gmail.com <joseph@>
2017-09-20 09:46:21 -0700

    Add back end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 //back-end logic
 var isLeapYear = function(input){
-  return false;
+  return input % 400 === 0 || input % 4 === 0 && input % 100 !== 0;
 }
-
-
-
 
 //front-end logic
 $(document).ready(function(){
   $("form").submit(function(event){
+    event.preventDefault()
     var input = parseInt($("input#user-input").val());
     var output = isLeapYear(input);
 
@@ -17,6 +15,5 @@ $(document).ready(function(){
     } else {
       $("#output").text(input + " is not a leap year. Sorry :(")
     }
-
   });
 });
